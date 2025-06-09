ba01ba22858b98d9e4e77d01029fe7975faff04f
omarmansor <omansor360@gmail.com>
2025-06-09 13:49:05 +0300

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,32 @@
+import { NgClass, NgIf } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [NgIf],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  isShown = signal(true);
+  isOpened = signal(false);
+  showSearch = signal(false);
+  showLink = signal(false);
+  displayNone() {
+    this.isShown.set(false);
+  }
+  //maxi :)
+  openMenu() {
+    this.isOpened.update((current: any) => !current);
+  }
+  openSearch() {
+    this.showSearch.set(!this.showSearch());
+    console.log(this.showSearch());
+  }
+  openLinks() {
+    this.showLink.update((current: any) => !current);
+    console.log(this.showLink());
+  }
+}
+
+//note1: img will hold the transtion
---
+++ src/app/shared/dynamic-proudcts/dynamic-proudcts.component.ts
@@ -0,0 +1,62 @@
+import { CommonModule, NgClass, NgFor } from '@angular/common';
+import { Component, input } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+export interface data {
+  name: string;
+  rate: number;
+  price: number;
+  img: string;
+  discount?: number;
+  mainPrice?: number;
+  discountPricePrecntge?: number;
+}
+@Component({
+  selector: 'app-dynamic-proudcts',
+  imports: [NgFor, CommonModule],
+  templateUrl: './dynamic-proudcts.component.html',
+  styleUrl: './dynamic-proudcts.component.scss',
+  animations: [
+    trigger('expandCollapse', [
+      state(
+        'collapsed',
+        style({ height: '0px', overflow: 'hidden', opacity: 0 })
+      ),
+      state('expanded', style({ height: '*', opacity: 1 })),
+      transition('collapsed <=> expanded', [animate('500ms ease')]),
+    ]),
+  ],
+})
+export class DynamicProudctsComponent {
+  header = input.required<string>();
+  products = input.required<data[]>();
+  showAll = false;
+  getStars(rate: number): ('full' | 'half' | 'empty')[] {
+    const full = Math.floor(rate);
+    const half = rate % 1 >= 0.25 && rate % 1 < 0.75;
+    const empty = 5 - full - (half ? 1 : 0);
+
+    return [
+      ...Array(full).fill('full'),
+      ...(half ? ['half'] : []),
+      ...Array(empty).fill('empty'),
+    ];
+  }
+
+  toggleView() {
+    this.showAll = !this.showAll;
+  }
+
+  get visibleProducts() {
+    return this.products().slice(0, 3);
+  }
+
+  get extraProducts() {
+    return this.products().slice(3);
+  }
+}
